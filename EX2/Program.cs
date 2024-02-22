c0194848b4327c39051d5c66241bad2aa5604d0b
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-02-21 22:52:26 -0300

    "add"

---
+++ EX2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0;
+            double somaN = 0;
+            double count = 0;
+            double result = 0;
+            
+            while (true)
+            {
+                Console.WriteLine("Digite um valor: ");
+                n = double.Parse(Console.ReadLine());
+
+                if (n < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    somaN += n;
+                    count++;
+                }
+            }
+             result = (count > 0)? somaN / count:0;
+
+            Console.WriteLine("A media dos numeros digitados é: {0}", result);
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setando Variaveis
+            int continuar = 0;
+            double n = 0;
+            double soma = 0;
+
+
+            //-------------------------
+
+
+            while(continuar <= 4) 
+            {
+                Console.Write("Digite um numero: ");
+                n = double.Parse(Console.ReadLine());
+                if(n>0)
+                {
+                    soma+=n;
+                    
+                }
+                else 
+                {
+                    Console.WriteLine("O número digitado é invalido, digite um numero positivo Por Favor!");
+                }
+
+                continuar++;
+
+            }
+
+            Console.WriteLine("O valor dos 5 n somados é {0}", soma);
+        }
+    }
+}
